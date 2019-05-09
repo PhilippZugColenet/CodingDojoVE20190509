fcd9ade169bf0ede6b4d6a599d5be49fb2b3b692
Salman Khan <salman.khan@conplement.de>
2019-05-09 15:10:29 +0200

    new interfaces for direction

--- kata.cpp
+++ kata.cpp
@@ -2,28 +2,48 @@
 
 class Rover
 {
-    uint m_maxCol;
-    uint m_maxRow;
-    uint m_curRow = 0;
-    uint m_curCol;
 public:
+    enum Direction {
+        North,
+        East
+    };
+
     Rover(uint maxrow, uint maxcol) : m_maxCol(maxcol), m_maxRow(maxrow)
     {}
 
     bool setPosition(uint x, uint y)
     {
       if (x < m_maxRow && y < m_maxCol) {
-        m_curRow = x;
-        m_curCol = y;
+        m_x = x;
+        m_y = y;
 
         return true;
       }
       return false;
     }
 
-    uint getPositionX() {
-      return m_curRow;
+    uint getPositionX() const {
+      return m_x;
     }
+
+    uint getPositionY() const {
+      return m_y;
+    }
+
+    Direction getDirection() const {
+        return m_dir;
+    }
+
+    void setDirection(Direction dir) {
+        m_dir = dir;
+    }
+
+private:
+    uint m_maxCol;
+    uint m_maxRow;
+    uint m_x = 0;
+    uint m_y = 0;
+    Direction m_dir = Direction::North;
 };
 
 TEST_GROUP(RoverTests) {
@@ -60,4 +80,30 @@ TEST(RoverTests, getInitialPositionX)
 {
   Rover rov(5,5);
   CHECK_EQUAL(0, rov.getPositionX());
+}
+
+TEST(RoverTests, getPositionY)
+{
+    Rover rov(5,5);
+    rov.setPosition(3, 3);
+    CHECK_EQUAL(3, rov.getPositionY());
+}
+
+TEST(RoverTests, getInitialPositionY)
+{
+  Rover rov(5,5);
+  CHECK_EQUAL(0, rov.getPositionY());
+}
+
+TEST(RoverTests, getDirection)
+{
+  Rover rov(5,5);
+  CHECK_EQUAL(Rover::Direction::North, rov.getDirection());
+}
+
+TEST(RoverTests, setDirection)
+{
+  Rover rov(5,5);
+  rov.setDirection(Rover::Direction::East);
+  CHECK_EQUAL(Rover::Direction::East, rov.getDirection());
 }
