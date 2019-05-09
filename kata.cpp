97ce0f3ecc467dfac22ee2c7b11ed7989f069ef6
Eduard Boitschenko <eduard.boitschenko@macio.de>
2019-05-09 14:39:06 +0200

    more test cases

--- kata.cpp
+++ kata.cpp
@@ -2,15 +2,27 @@
 
 class Rover
 {
-    int m_maxCol;
-    int m_maxRow;
+    uint m_maxCol;
+    uint m_maxRow;
+    uint m_curRow = 0;
+    uint m_curCol;
 public:
-    Rover(int maxrow, int maxcol) : m_maxCol(maxcol), m_maxRow(maxrow)
+    Rover(uint maxrow, uint maxcol) : m_maxCol(maxcol), m_maxRow(maxrow)
     {}
 
-    bool setPosition(int x, int y)
+    bool setPosition(uint x, uint y)
     {
+      if (x < m_maxRow && y < m_maxCol) {
+        m_curRow = x;
+        m_curCol = y;
+
         return true;
+      }
+      return false;
+    }
+
+    uint getPositionX() {
+      return m_curRow;
     }
 };
 
@@ -28,4 +40,24 @@ TEST(RoverTests, setPositionInsideBoundaries)
 
 }
 
+TEST(RoverTests, setPositionOutsideBoundariesShouldFail)
+{
+    Rover rov(5, 5);
+    CHECK_EQUAL(false, rov.setPosition(6, 4));
+    CHECK_EQUAL(false, rov.setPosition(4, 6));
+    CHECK_EQUAL(false, rov.setPosition(5, 5));
+    CHECK_EQUAL(false, rov.setPosition(6, 7));
+}
 
+TEST(RoverTests, getPositionX)
+{
+    Rover rov(5,5);
+    rov.setPosition(3, 3);
+    CHECK_EQUAL(3, rov.getPositionX());
+}
+
+TEST(RoverTests, getInitialPositionX)
+{
+  Rover rov(5,5);
+  CHECK_EQUAL(0, rov.getPositionX());
+}
